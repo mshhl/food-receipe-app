4e56a248c7fb8276b540b65861075090cecc1254
Mohammed Mishhal <mishhalstep2future@gmail.com>
2025-08-14 16:42:51 +0530

    FoodItem added latest version completed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 const URL = import.meta.env.VITE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-export default function Search() {
+export default function Search({foodData,setFoodData}) {
   const [query, setQuery] = useState("pizza");
   useEffect(() => {
     async function fetchFood() {
       const response = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
       const data = await response.json();
       console.log(data.results);
+      setFoodData(data.results);
     }
     fetchFood();
   }, [query]);
