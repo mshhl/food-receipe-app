184ee46c8ed7928deabcbb2aa455e99cf05664d0
Mohammed Mishhal <mishhalstep2future@gmail.com>
2025-09-21 14:16:29 +0530

    ingredients box shadow design over

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,18 @@ import FoodDetails from "./components/FoodDetails";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
-  const [foodId,setFoodId] = useState("52772");
+  const [foodId, setFoodId] = useState("52772");
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Container>
-       <InnerContainer>
-        <FoodList setFoodId={setFoodId} foodData={foodData} />
-       </InnerContainer>
-       <InnerContainer>
-        <FoodDetails foodId = {foodId}/>
-       </InnerContainer>
+        <InnerContainer>
+          <FoodList setFoodId={setFoodId} foodData={foodData} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
       </Container>
     </div>
   );
